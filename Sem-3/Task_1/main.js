aae45ff11f8a4bfa6c485c3e1b1c44bde8ede9a6
a.fedotov <zacyla@gmail.com>
2023-02-21 18:08:17 +0300

    zadacha 1(sem)

---
+++ Sem-3/Task_1/main.js
@@ -0,0 +1,9 @@
+const a = Number.parseFloat(promt("enter num 1: "));
+const b = Number.parseFloat(promt("enter num 2: "));
+const s = sum(a,b);
+
+alert (`Result summ ${a} + ${b} = ${s}`)
+
+function summ(x,y){
+    return x + y;
+}
